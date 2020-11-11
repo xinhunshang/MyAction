ddefd2cf8bde9a6289454d042cd650fd0c6566d3
xinhunshang <50572484+xinhunshang@users.noreply.github.com>
2020-11-12 07:49:18 +0800

    Update oneByOneExecute.js

--- oneByOneExecute.js
+++ oneByOneExecute.js
@@ -7,7 +7,7 @@ const smartReplace = require("./smartReplace");
 const Secrets = {
     JD_COOKIE: process.env.JD_COOKIE, //cokie,多个用&隔开即可
     SyncUrl: process.env.SYNCURL, //签到地址,方便随时变动
-    PUSH_KEY: '', //server酱推送消息
+    PUSH_KEY: process.env.PUSH_KEY, //server酱推送消息
     BARK_PUSH: process.env.BARK_PUSH, //Bark推送
     TG_BOT_TOKEN: process.env.TG_BOT_TOKEN, //TGBot推送Token
     TG_USER_ID: process.env.TG_USER_ID, //TGBot推送成员ID
